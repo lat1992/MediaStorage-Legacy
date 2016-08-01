21a5fcd71f5af6fbfeb3653868a2be1fef25828e
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-08-01 18:32:17 +0200

    add route

---
+++ Route.php
@@ -0,0 +1,27 @@
+<?php
+
+class Route {
+
+	protected $_route;
+
+	public function __construct() {
+		$this->_route = array();
+
+		$this->loadRoutes();
+	}
+
+	public function loadRoutes() {
+
+		require_once('CoreBundle/routes/route.php');
+	}
+
+	public function getController($page) {
+
+		foreach ($this->_route as $key => $value) {
+			if (strcmp($page, $value[0]) === 0)
+				return ($this->_route[$key]);
+		}
+		
+		return false;
+	}
+}
