0bc4c78f0b2a150accfcedd5d6ed704246648f59
mauhoi wu <mauhoi@capitalvision.fr>
2016-08-08 16:26:17 +0200

    add folder

--- ClientBundle/routes/route.php
+++ ClientBundle/routes/route.php
@@ -2,4 +2,6 @@
 
 $this->_route[] = array('home', 'ClientBundle/controllers/HomeController.php', 'HomeController', 'HomeAction');
 
+$this->_route[] = array('folderPage', 'ClientBundle/controllers/FolderPageController.php', 'FolderPageController', 'FolderPageAction');
+
 ?>
