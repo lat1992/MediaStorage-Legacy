abc954e7a1d92622bbb6763146c8a48da48950e1
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-08-08 18:36:56 +0200

    temp folder commit

--- ClientBundle/views/layout/footer.php
+++ ClientBundle/views/layout/footer.php
@@ -2,7 +2,7 @@
 			</div>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-		<script src="ClientBundle/ressources/libs/Slidebars/dev/slidebars.js"></script>
+		<script src="ClientBundle/ressources/libs/Slidebars/dist/slidebars.min.js"></script>
 		<script src="ClientBundle/ressources/layout/js/menu.js"></script>
 
 	</body>
