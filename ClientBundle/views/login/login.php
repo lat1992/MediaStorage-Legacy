e23a7d9450ebf749f120cbc7771bbcb3b6dc9d08
mauhoi wu <mauhoi@capitalvision.fr>
2016-08-09 11:56:21 +0200

    add login view

--- ClientBundle/views/login/login.php
+++ ClientBundle/views/login/login.php
@@ -3,26 +3,26 @@
     <meta charset="UTF-8">
     <title>MediaStorage</title>
     <link rel="stylesheet" type="text/css" href="ClientBundle/ressources/login/css/style.css" />
-    <script src="js/prefixfree.min.js"></script>
   </head>
 
   <body>
-    <div class="login-form">
-      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . '?' . $_SERVER['QUERY_STRING']); ?>" method="POST">
-        <div class="field">
-          <label for="username_mediastorage"><?= USERNAME ?></label>
-          <input type="text" name="username_mediastorage" id="username_mediastorage"/>
-        </div>
-        <div class="field">
-          <label for="password_mediastorage"><?= PASSWORD ?></label>
-          <input type="password" name="password_mediastorage" id="password_mediastorage"/>
-        </div>
-        <div class="field">
-        <input type="hidden" name="id_login_mediastorage" value="98374" />
-        <input type="submit" value="<?= VALIDATE ?>" />
-        </div>
-      </form>
+
+    <div class="login-page">
+        <div class= "logo" style='background-image: url(https://pbs.twimg.com/profile_images/1179925665/media365_400.jpg)'></div>
+      <div class="form">
+        <form class="login-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . '?' . $_SERVER['QUERY_STRING']); ?>" method="POST">
+          <input type="text" name="username_mediastorage" id="username_mediastorage" placeholder="<?= USERNAME ?>"/>
+          <input type="password" name="password_mediastorage" id="password_mediastorage" placeholder="<?= PASSWORD ?>"/>
+          <input type="hidden" name="id_login_mediastorage" value="98374" />
+          <button type="submit"><?= VALIDATE ?></button>
+        </form>
+      </div>
     </div>
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script>
+      $('.message a').click(function(){
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+      });
+    </script>
   </body>
 </html>
