970fe829c9118ae3495919e373e372c45c0191ae
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-08-09 10:23:04 +0200

    correction clic to close sidebar

--- ClientBundle/ressources/layout/js/menu.js
+++ ClientBundle/ressources/layout/js/menu.js
@@ -6,15 +6,17 @@
 
     $( '.js-open-left-slidebar' ).on( 'click', function ( event ) {
         event.stopPropagation();
-        controller.open( 'slidebar-1' );
+        controller.toggle( 'slidebar-1' );
     } );
 
     $( controller.events ).on( 'opened', function () {
         $( '[canvas="container"]' ).addClass( 'js-close-slidebar' );
+        $( 'nav' ).addClass( 'js-close-slidebar' );
     } );
 
     $( controller.events ).on( 'closed', function () {
         $( '[canvas="container"]' ).removeClass( 'js-close-slidebar' );
+        $( 'nav' ).removeClass( 'js-close-slidebar' );
     } );
 
     $( 'body' ).on( 'click', '.js-close-slidebar', function ( event ) {
