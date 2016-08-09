d255b0a5efc8af0ef6cd58561ec4548f2bd83c9a
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-08-09 15:44:28 +0200

    clean graphical part

--- ClientBundle/ressources/layout/js/menu.js
+++ ClientBundle/ressources/layout/js/menu.js
@@ -24,4 +24,39 @@
         controller.close();
     } );
 
+/*
+
+    $( '.js-open-left-slidebar' ).on( 'click', function ( event ) {
+        event.stopPropagation();
+        controller.toggle( 'slidebar-1' );
+    } );
+
+    $( controller.events ).on( 'opened', function () {
+//        $( '[canvas="container"]' ).addClass( 'js-close-slidebar' );
+        $( 'nav' ).addClass( 'js-close-slidebar' );
+
+        var $width = $(document).width() - $('[off-canvas]').width();
+
+        $( '[canvas="container"]' ).css('width', $width);
+        $( 'nav' ).css('width', $width);
+    } );
+
+    $( controller.events ).on( 'closed', function () {
+        $( '[canvas="container"]' ).removeClass( 'js-close-slidebar' );
+        $( 'nav' ).removeClass( 'js-close-slidebar' );
+
+        var $width = $(document).width();
+
+        $( '[canvas="container"]' ).css('width', $width);
+        $( 'nav' ).css('width', $width);
+    } );
+
+    // $( 'body' ).on( 'click', '.js-close-slidebar', function ( event ) {
+    //     event.stopPropagation();
+    //     controller.close();
+    // } );
+
+*/
+
+
 } ) ( jQuery );
