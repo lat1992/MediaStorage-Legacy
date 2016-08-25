8eac67ce010c6cc17cab060112e7fb32c7345bd4
LAT <lat@live.hk>
2016-08-25 15:55:09 +0200

    clean

--- ClientBundle/views/program/program.php
+++ ClientBundle/views/program/program.php
@@ -17,7 +17,7 @@ require_once('ClientBundle/views/layout/header.php');
             <a class="link_div_program" href="?page=program&media_id=<?= $program['id']; ?>">
             <div class="col hvr-grow program">
                 <div class="title_div"><span><h2><?= $program['reference'] ?></h2></span></div>
-                <img src="ClientBundle/ressources/media/images/program/<?= $_SESSION['id_organization'] ?>/default.png" />
+                <img src="ClientBundle/ressources/media/images/program/default.png" />
                 <div>
                 <!-- TODO: DESCIPTION OF PROGRAMME HERE -->
                 </div>
@@ -35,7 +35,7 @@ require_once('ClientBundle/views/layout/header.php');
             <a class="link_div_content" href="?page=content&media_id=<?= $content['id']; ?>">
             <div class="col hvr-grow content">
                 <div class="title_div"><span><h2><?= $content['reference'] ?></h2></span></div>
-                <img src="ClientBundle/ressources/media/images/content/<?= $_SESSION['id_organization'] ?>/default.png" />
+                <img src="ClientBundle/ressources/media/images/content/default.png" />
                 <div>
                 </div>
 
