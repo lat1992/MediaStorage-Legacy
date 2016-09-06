bfde8732eb5d04c9a947491f149bda5a8a544297
Mau Hoi WU <mauhoi.wu@gmail.com>
2016-09-06 11:00:22 +0200

    add english translation

--- translation/language_en_US.php
+++ translation/language_en_US.php
@@ -30,19 +30,19 @@ define('INVALID_JOB_TOO_LONG', 'Job name is too long');
 define('INVALID_DATA_TOO_LONG', 'Data is too long');
 define('INVALID_REFERENCE_TOO_LONG', 'Reference is too long');
 define('INVALID_NAME_TOO_LONG', 'Name is too long');
-define('INVALID_FILESERVER_TOO_LONG', '');
-define('INVALID_CODE_TOO_LONG', '');
-define('INVALID_EMAIL_TOO_LONG', '');
-define('INVALID_PARENT_ID', '');
-define('INVALID_EPISODE_NUMBER_TOO_LONG', '');
-define('INVALID_IMAGE_VERSION_DATA_TOO_LONG', '');
-define('INVALID_SOUND_VERSION_DATA_TOO_LONG', '');
-define('INVALID_ELEMENT_TOO_LONG', '');
-define('INVALID_TYPE_TOO_LONG', '');
-define('INVALID_FILENAME_TOO_LONG', '');
-define('INVALID_FILEPATH_TOO_LONG', '');
-define('INVALID_TRANSLATE_TOO_LONG', '');
-define('INVALID_MEDIA_REFERENCE_TOO_LONG', '');
+define('INVALID_FILESERVER_TOO_LONG', 'File server name is too long');
+define('INVALID_CODE_TOO_LONG', 'Code is too long');
+define('INVALID_EMAIL_TOO_LONG', 'Email address is too long');
+define('INVALID_PARENT_ID', 'Parent ID invalid');
+define('INVALID_EPISODE_NUMBER_TOO_LONG', 'Too much charactere for episode number');
+define('INVALID_IMAGE_VERSION_DATA_TOO_LONG', 'Too much charactere for image version');
+define('INVALID_SOUND_VERSION_DATA_TOO_LONG', 'Too much charactere for sound version');
+define('INVALID_ELEMENT_TOO_LONG', 'Too much charactere for element');
+define('INVALID_TYPE_TOO_LONG', 'Too much charactere for type');
+define('INVALID_FILENAME_TOO_LONG', 'File name is too long');
+define('INVALID_FILEPATH_TOO_LONG', 'File path is too long');
+define('INVALID_TRANSLATE_TOO_LONG', 'Translation is too long');
+define('INVALID_MEDIA_REFERENCE_TOO_LONG', 'Reference is too long');
 
 // Role
 define('ADMINISTRATOR', 'Administrator');
@@ -96,84 +96,84 @@ define('FOLDER_LIST_TITLE', 'Folder list');
 define('FOLDER_CREATION_TITLE', 'Add folder');
 
 // Media
-define('MEDIA', '');
-define('MEDIA_PARENT', '');
-define('FOLDER_PARENT', '');
-define('INFO_MOVE_DIRECTORY', '');
-define('REFERENCE', '');
-define('REFERENCE_CLIENT', '');
-define('HANDOVER_DATE', '');
-define('RIGHT_VIEW', '');
+define('MEDIA', 'Media');
+define('MEDIA_PARENT', 'Parent media');
+define('FOLDER_PARENT', 'Parent folder');
+define('INFO_MOVE_DIRECTORY', 'Leave blank if not nessessary');
+define('REFERENCE', 'Reference');
+define('REFERENCE_CLIENT', 'Client\'s reference');
+define('HANDOVER_DATE', 'Handover date');
+define('RIGHT_VIEW', 'Right viewing');
 
 // Media info
-define('MEDIA_INFO', '');
-define('TITLE', '');
-define('SUBTITLE', '');
-define('DESCRIPTION', '');
-define('EPISODE_NUMBER', '');
-define('IMAGE_VERSION', '');
-define('SOUND_VERSION', '');
+define('MEDIA_INFO', 'Media information');
+define('TITLE', 'Title');
+define('SUBTITLE', 'Subtitle');
+define('DESCRIPTION', 'Description');
+define('EPISODE_NUMBER', 'Episode number');
+define('IMAGE_VERSION', 'Image version');
+define('SOUND_VERSION', 'Sound version');
 
 // Program
-define('PROGRAM', '');
-define('PROGRAM_LIST_TITLE', '');
-define('CREATE_MEDIA_PROGRAM', '');
-define('EDIT_MEDIA_PROGRAM', '');
+define('PROGRAM', 'Program');
+define('PROGRAM_LIST_TITLE', 'Program list');
+define('CREATE_MEDIA_PROGRAM', 'Add program');
+define('EDIT_MEDIA_PROGRAM', 'Edit Program');
 
 // Content
-define('CONTENT', '');
-define('CONTENT_LIST_TITLE', '');
-define('CREATE_MEDIA_CONTENT', '');
-define('EDIT_MEDIA_CONTENT', '');
+define('CONTENT', 'Content');
+define('CONTENT_LIST_TITLE', 'Content list');
+define('CREATE_MEDIA_CONTENT', 'Add content');
+define('EDIT_MEDIA_CONTENT', 'Edit content');
 
 // Media file
-define('FILENAME', '');
-define('RIGHT_DOWNLOAD', '');
-define('RIGHT_ADDTOCART', '');
+define('FILENAME', 'File name');
+define('RIGHT_DOWNLOAD', 'Right downloading');
+define('RIGHT_ADDTOCART', 'Right adding to cart');
 
 // Field
-define('FIELD', '');
-define('MEDIA_EXTRA_FIELD_LIST_TITLE', '');
-define('MEDIA_EXTRA_FIELD_CREATION_TITLE', '');
-define('MEDIA_EXTRA_FIELD_EDIT_TITLE', '');
-define('TYPE', '');
-define('TEXT', '');
-define('DATE', '');
-define('ARRAY_MUTIPLE', '');
-define('ARRAY_UNIQUE', '');
-define('T_BOOLEAN', '');
-define('MANDATORY', '');
+define('FIELD', 'Field');
+define('MEDIA_EXTRA_FIELD_LIST_TITLE', 'Field list');
+define('MEDIA_EXTRA_FIELD_CREATION_TITLE', 'Add Field');
+define('MEDIA_EXTRA_FIELD_EDIT_TITLE', 'Edit Field');
+define('TYPE', 'Type');
+define('TEXT', 'Text');
+define('DATE', 'Date');
+define('ARRAY_MUTIPLE', 'Array mutiple choice');
+define('ARRAY_UNIQUE', 'Array single choice');
+define('T_BOOLEAN', 'Check box');
+define('MANDATORY', 'Mandatory');
 
 // Group
-define('GROUP', '');
-define('GROUP_LIST_TITLE', '');
-define('GROUP_CREATION_TITLE', '');
-define('GROUP_EDIT_TITLE', '');
-define('FILESERVER', '');
-define('NB_ORGANIZATION', '');
+define('GROUP', 'Group');
+define('GROUP_LIST_TITLE', 'Group list');
+define('GROUP_CREATION_TITLE', 'Add group');
+define('GROUP_EDIT_TITLE', 'Edit group');
+define('FILESERVER', 'File server');
+define('NB_ORGANIZATION', 'Number of organization');
 
 // Organization
-define('ORGANIZATION', '');
-define('ORGANIZATION_LIST_TITLE', '');
-define('ORGANIZATION_CREATION_TITLE', '');
-define('ORGANIZATION_EDIT_TITLE', '');
+define('ORGANIZATION', 'Organization');
+define('ORGANIZATION_LIST_TITLE', 'Organization list');
+define('ORGANIZATION_CREATION_TITLE', 'Add organization');
+define('ORGANIZATION_EDIT_TITLE', 'Edit organization');
 
 // User
-define('USER', '');
-define('USER_LIST_TITLE', '');
-define('USER_CREATION_TITLE', '');
-define('USER_EDIT_TITLE', '');
+define('USER', 'User');
+define('USER_LIST_TITLE', 'User list');
+define('USER_CREATION_TITLE', 'Add user');
+define('USER_EDIT_TITLE', 'Edit user');
 
 // Role
-define('ROLE', '');
-define('ROLE_LIST_TITLE', '');
-define('ROLE_CREATION_TITLE', '');
-define('ROLE_EDIT_TITLE', '');
-define('NB_PERMIT', '');
-define('PERMIT', '');
+define('ROLE', 'Role');
+define('ROLE_LIST_TITLE', 'Role list');
+define('ROLE_CREATION_TITLE', 'Add role');
+define('ROLE_EDIT_TITLE', 'Edit role');
+define('NB_PERMIT', 'Number of permit');
+define('PERMIT', 'Permit');
 
 // Email List
 define('EMAIL', 'EMAIL');
-define('MAIL_LIST_TITLE', '');
-define('MAIL_CREATION_TITLE', '');
-define('MAIL_EDIT_TITLE', '');
+define('MAIL_LIST_TITLE', 'Mail list');
+define('MAIL_CREATION_TITLE', 'Add mail');
+define('MAIL_EDIT_TITLE', 'Edit mail');
