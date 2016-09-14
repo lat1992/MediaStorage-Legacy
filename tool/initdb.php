7dcda15258b8c781fba8e88379e5f80d4ddc1b5c
LAT <lat@live.hk>
2016-09-14 12:57:30 +0200

    change memory structure

--- tool/initdb.php
+++ tool/initdb.php
@@ -6,72 +6,65 @@
 
 	// chapter_language
 	$mysqli->query('TRUNCATE TABLE memory_chapter_language');
-	$result = $mysqli->query('SELECT id, data FROM chapter_language WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_chapter, id_language, data FROM chapter_language WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_chapter_language (id, data) VALUES ('. $row['id'] .', "'. $row['data'] .'");');
+		$mysqli->query('INSERT INTO memory_chapter_language (id, id_chapter, id_language, data) VALUES ('. $row['id'] .', '. $row['id_chapter'] .', '. $row['id_language'] .', "'. $row['data'] .'");');
 	}
 
 	// folder_language
 	$mysqli->query('TRUNCATE TABLE memory_folder_language');
-	$result = $mysqli->query('SELECT id, data, description FROM folder_language WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_folder, id_language, data, description FROM folder_language WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_folder_language (id, data, description) VALUES ('. $row['id'] .', "'. $row['data'] .'", "'. $row['description'] .'");');
+		$mysqli->query('INSERT INTO memory_folder_language (id, id_folder, id_language, data, description) VALUES ('. $row['id'] .', '. $row['id_folder'] .', '. $row['id_language'] .', "'. $row['data'] .'", "'. $row['description'] .'");');
 	}
 
 	// media
 	$mysqli->query('TRUNCATE TABLE memory_media');
-	$result = $mysqli->query('SELECT id, reference, reference_client FROM media WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_type, id_organization, reference, reference_client, right_view FROM media WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_media (id, reference, reference_client) VALUES ('. $row['id'] .', "'. $row['reference'] .'", "'. $row['reference_client'] .'");');
+		$mysqli->query('INSERT INTO memory_media (id, id_type, id_organization, reference, reference_client, right_view) VALUES ('. $row['id'] .', '. $row['id_type'] .', '. $row['id_organization'] .', "'. $row['reference'] .'", "'. $row['reference_client'] .'", '. $row['right_view'] .');');
 	}
 
 	// media_extra
 	$mysqli->query('TRUNCATE TABLE memory_media_extra');
-	$result = $mysqli->query('SELECT id, data FROM media_extra WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_media, id_field, id_language, id_array, data FROM media_extra WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_media_extra (id, data) VALUES ('. $row['id'] .', "'. $row['data'] .'");');
+		$mysqli->query('INSERT INTO memory_media_extra (id, id_media, id_field, id_language, id_array, data) VALUES ('. $row['id'] .', '. $row['id_media'] .', '. $row['id_field'] .', '. $row['id_language'] .', '. $row['id_array'] .', "'. $row['data'] .'");');
 	}
 
 	// media_extra_array
 	$mysqli->query('TRUNCATE TABLE memory_media_extra_array');
-	$result = $mysqli->query('SELECT id, element FROM media_extra_array WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_field, id_language, element FROM media_extra_array WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_media_extra_array (id, element) VALUES ('. $row['id'] .', "'. $row['element'] .'");');
+		$mysqli->query('INSERT INTO memory_media_extra_array (id, id_field, id_language, element) VALUES ('. $row['id'] .', '. $row['id_field'] .', '. $row['id_language'] .', "'. $row['element'] .'");');
 	}
 
 	// media_extra_field_language
 	$mysqli->query('TRUNCATE TABLE memory_media_extra_field_language');
-	$result = $mysqli->query('SELECT id, data FROM media_extra_field_language WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_field, id_language, data FROM media_extra_field_language WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_media_extra_field_language (id, data) VALUES ('. $row['id'] .', "'. $row['data'] .'");');
+		$mysqli->query('INSERT INTO memory_media_extra_field_language (id, id_field, id_language, data) VALUES ('. $row['id'] .', '. $row['id_field'] .', '. $row['id_language'] .', "'. $row['data'] .'");');
 	}
 
 	// media_file
 	$mysqli->query('TRUNCATE TABLE memory_media_file');
-	$result = $mysqli->query('SELECT id, filename FROM media_file WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_media, filename, right_download, right_addtocart FROM media_file WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_media_file (id, filename) VALUES ('. $row['id'] .', "'. $row['filename'] .'");');
+		$mysqli->query('INSERT INTO memory_media_file (id, id_media, filename, right_download, right_addtocart) VALUES ('. $row['id'] .', '. $row['id_media'] .', "'. $row['filename'] .'", '. $row['right_download'] .', '. $row['right_addtocart'] .');');
 	}
 
 	// media_info
 	$mysqli->query('TRUNCATE TABLE memory_media_info');
-	$result = $mysqli->query('SELECT id, title, subtitle FROM media_info WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_media, id_language, title, subtitle FROM media_info WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_media_info (id, title, subtitle, episode_number, image_version, sound_version) VALUES ('. $row['id'] .', "'. $row['title'] .'", "'. $row['subtitle'] .'");');
-	}
-
-	// tag
-	$mysqli->query('TRUNCATE TABLE memory_tag');
-	$result = $mysqli->query('SELECT id, tag FROM tag WHERE 1;');
-	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_tag (id, tag) VALUES ('. $row['id'] .', "'. $row['tag'] .'");');
+		$mysqli->query('INSERT INTO memory_media_info (id, id_media, id_language, title, subtitle) VALUES ('. $row['id'] .', '. $row['id_media'] .', '. $row['id_language'] .', "'. $row['title'] .'", "'. $row['subtitle'] .'");');
 	}
 
 	// tag_language
 	$mysqli->query('TRUNCATE TABLE memory_tag_language');
-	$result = $mysqli->query('SELECT id, data FROM tag_language WHERE 1;');
+	$result = $mysqli->query('SELECT id, id_tag, id_language, data FROM tag_language WHERE 1;');
 	while ($row = $result->fetch_assoc()) {
-		$mysqli->query('INSERT INTO memory_tag_language (id, data) VALUES ('. $row['id'] .', "'. $row['data'] .'");');
+		$mysqli->query('INSERT INTO memory_tag_language (id, id_tag, id_language, data) VALUES ('. $row['id'] .', '. $row['id_tag'] .', '. $row['id_language'] .', "'. $row['data'] .'");');
 	}
 
 	$mysqli->close();
