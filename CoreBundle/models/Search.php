2ff06236dfea9c21ef7c9351d6f4d15c5653c46c
LAT <lat@live.hk>
2016-09-19 10:09:38 +0200

    search model finish

--- CoreBundle/models/Search.php
+++ CoreBundle/models/Search.php
@@ -49,14 +49,14 @@ class Search extends Model {
 			' JOIN memory_chapter_language ON chapter.id = memory_chapter_language.id_chapter'.
 			' JOIN folder ON folder.id_media = memory_media.id'.
 			' JOIN memory_folder_language ON memory_folder_language.id_folder = folder.id'.
-			' JOIN memory_media_extra ON memory_media_extra.id_media = memory_media.id'.
 			' JOIN media_extra_field ON media_extra_field.id_organization = memory_media.id_organization'.
+			' JOIN memory_media_extra ON memory_media_extra.id_media = memory_media.id AND memory_media_extra.id_field = media_extra_field.id'.
 			' JOIN memory_media_extra_array ON memory_media_extra_array.id_field = media_extra_field.id'.
 			' JOIN memory_media_file ON memory_media_file.id_media = memory_media.id'.
 			' JOIN tag ON tag.id_media = memory_media.id'.
 			' JOIN memory_tag_language ON memory_tag_language.id_tag = tag.id'.
 			' WHERE memory_media.id_type = '.$id_type.' AND memory_media.id_organization = '.$id_organization.' AND memory_media_info.id_language = '.$id_language.' AND memory_chapter_language.id_language = '.$id_language.' AND memory_folder_language.id_language = '.$id_language.' AND memory_media_extra.id_language = '.$id_language.' AND memory_media_extra_array.id_language = '.$id_language.' AND memory_tag_language.id_language = '.$id_language.' AND '
-			'(memory_chapter_language.data LIKE "%'.$keyword.'%" OR memory_folder_language.data LIEK "%'.$keyword.'%" OR memory_media.reference IS '.$keyword.' OR memory_media.reference_client LIKE '.$keyword.' OR memory_media_extra.data LIKE "%'.$keyword.'%" OR memory_media_extra_array.element LIKE "%'.$keyword.'%" OR memory_media_file.filename LIKE "%'.$keyword.'%" OR memory_media_info.title LIKE "%'.$keyword.'%" OR memory_media_info.subtitle LIKE "%'.$keyword.'%" OR media_info.description LIKE "%'.$keyword.'%" OR memory_tag_language.data LIKE "%'.$keyword.'%")'
+			'(memory_chapter_language.data LIKE "%'.$keyword.'%" OR memory_folder_language.data LIEK "%'.$keyword.'%" OR memory_media.reference IS '.$keyword.' OR memory_media.reference_client LIKE "'.$keyword.'" OR memory_media_extra.data LIKE "%'.$keyword.'%" OR memory_media_extra_array.element LIKE "%'.$keyword.'%" OR memory_media_file.filename LIKE "%'.$keyword.'%" OR memory_media_info.title LIKE "%'.$keyword.'%" OR memory_media_info.subtitle LIKE "%'.$keyword.'%" OR media_info.description LIKE "%'.$keyword.'%" OR memory_tag_language.data LIKE "%'.$keyword.'%")'
 			);
 
 		return array(
@@ -142,6 +142,20 @@ class Search extends Model {
 			);
 	}
 
+	public function searchMediaWithMediaInfoDescription($keyword, $id_type, $id_organization, $id_language) {
+		$data = $this->_mysqli->query('SELECT DISTINCT(memory_media.id), memory_media_info.title, memory_media_info.subtitle, media_info.description FROM  memory_media_info'.
+			' JOIN media_info.id ON media_info.id = memory_media_info.id'.
+			' JOIN memory_media ON memory_media.id = media_info.id_media'.
+			' WHERE memory_media.id_type = '.$id_type.' AND memory_media.id_organization = '.$id_organization.' AND memory_media_info.id_language = '.$id_language.' AND '
+			'(media_info.description LIKE "%'.$keyword.'%")'
+			);
+
+		return array(
+			'data' => $data,
+			'error' => ($this->_mysqli->error) ? 'searchMediaWithMediaInfoDescription: ' . $this->_mysqli->error : '',
+			);
+	}
+
 	public function searchMediaWithMediaExtra($keyword, $id_type, $id_organization, $id_language) {
 		$data = $this->_mysqli->query('SELECT DISTINCT(memory_media.id), memory_media_info.title, memory_media_info.subtitle, media_info.description FROM  memory_media_info'.
 			' JOIN media_info.id ON media_info.id = memory_media_info.id'.
@@ -153,7 +167,67 @@ class Search extends Model {
 
 		return array(
 			'data' => $data,
-			'error' => ($this->_mysqli->error) ? 'searchMedia: ' . $this->_mysqli->error : '',
+			'error' => ($this->_mysqli->error) ? 'searchMediaWithMediaExtra: ' . $this->_mysqli->error : '',
+			);
+	}
+
+	public function searchMediaWithMediaExtraArray($keyword, $id_type, $id_organization, $id_language) {
+		$data = $this->_mysqli->query('SELECT DISTINCT(memory_media.id), memory_media_info.title, memory_media_info.subtitle, media_info.description FROM  memory_media_info'.
+			' JOIN media_info.id ON media_info.id = memory_media_info.id'.
+			' JOIN memory_media ON memory_media.id = media_info.id_media'.
+			' JOIN media_extra_field ON media_extra_field.id_organization = memory_media.id_organization'.
+			' JOIN memory_media_extra ON memory_media_extra.id_media = memory_media.id AND memory_media_extra.id_field = media_extra_field.id'.
+			' JOIN memory_media_extra_array ON memory_media_extra_array.id_field = media_extra_field.id'.
+			' WHERE memory_media.id_type = '.$id_type.' AND memory_media.id_organization = '.$id_organization.' AND memory_media_extra_array.id_language = '.$id_language.' AND '
+			'(memory_media_extra_array.element LIKE "%'.$keyword.'%")'
+			);
+
+		return array(
+			'data' => $data,
+			'error' => ($this->_mysqli->error) ? 'searchMediaWithMediaExtraArray: ' . $this->_mysqli->error : '',
+			);
+	}
+
+	public function searchMediaWithMediaFile($keyword, $id_type, $id_organization, $id_language) {
+		$data = $this->_mysqli->query('SELECT DISTINCT(memory_media.id), memory_media_info.title, memory_media_info.subtitle, media_info.description FROM  memory_media_info'.
+			' JOIN media_info.id ON media_info.id = memory_media_info.id'.
+			' JOIN memory_media ON memory_media.id = media_info.id_media'.
+			' JOIN memory_media_file ON memory_media_file.id_media = memory_media.id'.
+			' WHERE memory_media.id_type = '.$id_type.' AND memory_media.id_organization = '.$id_organization.' '
+			'(memory_media_file.filename LIKE "%'.$keyword.'%")'
+			);
+
+		return array(
+			'data' => $data,
+			'error' => ($this->_mysqli->error) ? 'searchMediaWithMediaFile: ' . $this->_mysqli->error : '',
+			);
+	}
+
+	public function searchMediaWithMediaReference($keyword, $id_type, $id_organization, $id_language) {
+		$data = $this->_mysqli->query('SELECT DISTINCT(memory_media.id), memory_media_info.title, memory_media_info.subtitle, media_info.description FROM  memory_media_info'.
+			' JOIN media_info.id ON media_info.id = memory_media_info.id'.
+			' JOIN memory_media ON memory_media.id = media_info.id_media'.
+			' WHERE memory_media.id_type = '.$id_type.' AND memory_media.id_organization = '.$id_organization.' AND '
+			'(memory_media.reference IS '.$keyword.')'
+			);
+
+		return array(
+			'data' => $data,
+			'error' => ($this->_mysqli->error) ? 'searchMediaWithMediaReference: ' . $this->_mysqli->error : '',
+			);
+	}
+
+	public function searchMediaWithMediaReferenceClient($keyword, $id_type, $id_organization, $id_language) {
+		$data = $this->_mysqli->query('SELECT DISTINCT(memory_media.id), memory_media_info.title, memory_media_info.subtitle, media_info.description FROM  memory_media_info'.
+			' JOIN media_info.id ON media_info.id = memory_media_info.id'.
+			' JOIN memory_media ON memory_media.id = media_info.id_media'.
+			' WHERE memory_media.id_type = '.$id_type.' AND memory_media.id_organization = '.$id_organization.' AND '
+			'(memory_media.reference_client LIKE "'.$keyword.'")'
+			);
+
+		return array(
+			'data' => $data,
+			'error' => ($this->_mysqli->error) ? 'searchMediaWithMediaReferenceClient: ' . $this->_mysqli->error : '',
 			);
 	}
 
