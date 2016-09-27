16be49e0b635bd53db45dac11eeb1f6a44551253
Mau Hoi WU <mauhoi.wu@gmail.com>
2016-09-27 09:41:53 +0200

    transco module: send part

--- ToolBundle/managers/WorkFlowManager.php
+++ ToolBundle/managers/WorkFlowManager.php
@@ -12,10 +12,12 @@ class WorkFlowManager {
 
 	public function transcoding($id_file, $file_path) {
 		$wf_type = mime_content_type($file_path);
-		if (strcmp($wf_type, 'video/mp4'))
+		if (strpos($wf_type, 'video'))
 			return $this->_workFlowModel->transcodingVideo($id_file, pathinfo($file_path, PATHINFO_FILENAME), pathinfo($file_path, PATHINFO_DIRNAME), $_SESSION['id_organization']);
-		else if (strcmp($wf_type, 'image/jpg'))
+		else if (strpos($wf_type, 'image'))
 			return $this->_workFlowModel->transcodingImage($id_file, pathinfo($file_path, PATHINFO_FILENAME), pathinfo($file_path, PATHINFO_DIRNAME), $_SESSION['id_organization']);
+		else if (strpos($wf_type, 'audio'))
+			return $this->_workFlowModel->transcodingAudio($id_file, pathinfo($file_path, PATHINFO_FILENAME), pathinfo($file_path, PATHINFO_DIRNAME), $_SESSION['id_organization']);
 		else
 			return $this->_workFlowModel->transcodingOther($id_file, pathinfo($file_path, PATHINFO_FILENAME), pathinfo($file_path, PATHINFO_DIRNAME), $_SESSION['id_organization']);
 	}
--- ToolBundle/models/WorkFlow.php
+++ ToolBundle/models/WorkFlow.php
@@ -12,22 +12,24 @@ class WorkFlowModel extends Model {
 		parent::__construct('workflow');
 
 		$this->_workflow_addr = 'http://46.218.167.91:8080/mw/wf_main_put.php';
-		$this->_output_dir = '/var/www/html/mediastorage/file/';
+		$this->_output_dir = '/var/www/html/mediastorage/upload/file/';
 		$this->_final_path = '/var/www/html/mediastorage/archive/';
 	}
 
 	public function transcodingVideo($id_file, $input_file, $input_dir, $id_organization) {
+		$data = $this->_mysqli->query('SELECT workflow_code FROM workflow_organization WHERE transcoding_type = "video"');
+		$profile = $data->fetch_assoc();
 		$data = $this->_mysqli->query('INSERT INTO '.$this->_table.
-			' (id_media_file, transcoding_type) VALUES'.
+			' (id_media_file, id_organizatoin, transcoding_type) VALUES'.
 			' ('.$id_file.', "video")');
 		$post = array(
-			'task_id_' => $data->num_rows,
+			'order_id_' => $data->insert_id,
 			'file_in_' => $input_file,
-			'path_in_' => $input_dir,
+			'path_in_' => $input_dir.'/',
 			'file_out_' => $input_file,
 			'path_out_' => $this->_output_dir.$id_organization.'/',
-			'path_final_' => $this->_final_path,
-			'wfcode' => 'wf_video',
+			'path_final_' => $this->_final_path.$id_organization.'/',
+			'wfcode' => ($profile['workflow_code'] ? $profile['workflow_code'] : 'ms_video_default'),
 			'validwf' => 'ok'
 		);
 		return array(
@@ -37,17 +39,19 @@ class WorkFlowModel extends Model {
 	}
 
 	public function transcodingImage($id_file, $input_file, $input_dir, $id_organization) {
+		$data = $this->_mysqli->query('SELECT workflow_code FROM workflow_organization WHERE transcoding_type = "image"');
+		$profile = $data->fetch_assoc();
 		$data = $this->_mysqli->query('INSERT INTO '.$this->_table.
 			' (id_media_file, transcoding_type) VALUES'.
 			' ('.$id_file.', "image")');
 		$post = array(
-			'task_id_' => $data->num_rows,
+			'order_id_' => $data->insert_id,
 			'file_in_' => $input_file,
-			'path_in_' => $input_dir,
+			'path_in_' => $input_dir.'/',
 			'file_out_' => $input_file,
 			'path_out_' => $this->_output_dir.$id_organization.'/',
-			'path_final_' => $this->_final_path,
-			'wfcode' => 'wf_image',
+			'path_final_' => $this->_final_path.$id_organization.'/',
+			'wfcode' => ($profile['workflow_code'] ? $profile['workflow_code'] : 'ms_image_default'),
 			'validwf' => 'ok'
 		);
 		return array(
@@ -56,18 +60,42 @@ class WorkFlowModel extends Model {
 		);
 	}
 
+	public function transcodingAudio($id_file, $input_file, $input_dir, $id_organization) {
+		$data = $this->_mysqli->query('SELECT workflow_code FROM workflow_organization WHERE transcoding_type = "audio"');
+		$profile = $data->fetch_assoc();
+		$data = $this->_mysqli->query('INSERT INTO '.$this->_table.
+			' (id_media_file, transcoding_type) VALUES'.
+			' ('.$id_file.', "audio")');
+		$post = array(
+			'order_id_' => $data->insert_id,
+			'file_in_' => $input_file,
+			'path_in_' => $input_dir.'/',
+			'file_out_' => $input_file,
+			'path_out_' => $this->_output_dir.$id_organization.'/',
+			'path_final_' => $this->_final_path.$id_organization.'/',
+			'wfcode' => ($profile['workflow_code'] ? $profile['workflow_code'] : 'ms_audio_default'),
+			'validwf' => 'ok'
+		);
+		return array(
+			'data' => exec_post($post),
+			'error' => ($this->_mysqli->error) ? 'transcodingAudio: ' . $this->_mysqli->error : '',
+		);
+	}
+
 	public function transcodingOther($id_file, $input_file, $input_dir, $id_organization) {
+		$data = $this->_mysqli->query('SELECT workflow_code FROM workflow_organization WHERE transcoding_type = "other"');
+		$profile = $data->fetch_assoc();
 		$data = $this->_mysqli->query('INSERT INTO '.$this->_table.
 			' (id_media_file, transcoding_type) VALUES'.
 			' ('.$id_file.', "other")');
 		$post = array(
-			'task_id_' => $data->num_rows,
+			'order_id_' => $data->insert_id,
 			'file_in_' => $input_file,
-			'path_in_' => $input_dir,
+			'path_in_' => $input_dir.'/',
 			'file_out_' => $input_file,
 			'path_out_' => $this->_output_dir.$id_organization.'/',
-			'path_final_' => $this->_final_path,
-			'wfcode' => 'wf_other',
+			'path_final_' => $this->_final_path.$id_organization.'/',
+			'wfcode' => ($profile['workflow_code'] ? $profile['workflow_code'] : 'ms_other_default'),
 			'validwf' => 'ok'
 		);
 		return array(
@@ -76,9 +104,15 @@ class WorkFlowModel extends Model {
 		);
 	}
 
-	public function postProduction($task_id, $json) {
+	public function postProduction($order_id, $json) {
 		//mise à jour base de donnée
 		//$this->_mysqli->query();
+		$this->_mysqli->query('DELETE FROM '.$this->_table.' WHERE id = '.$order_id);
+
+		return array(
+			'data' => 'postProduction',
+			'error' => ($this->_mysqli->error) ? 'postProduction: ' . $this->_mysqli->error : '',
+		);
 	}
 
 	private function exec_post($post) {
