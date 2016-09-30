cd2f2f8ab4bd8a1fd464991b33dff801268d30ca
Mau Hoi WU <mauhoi.wu@gmail.com>
2016-09-30 15:45:49 +0200

    download list

--- ClientBundle/views/cart/cart_download_list.php
+++ ClientBundle/views/cart/cart_download_list.php
@@ -1,4 +1,30 @@
-<div>
+<?php
+
+require_once('ClientBundle/views/layout/header.php');
+
+?>
+
+<link rel="stylesheet" href="CoreBundle/ressources/layout/css/form.css">
+
+<style>
+
+<?php
+    if (isset($designs)) {
+
+        foreach ($designs as $design) {
+?>
+            <?= $design['selector'] ?> {
+                <?= $design['property'] ?> : <?= $design['value'] ?>;
+            }
+<?php
+        }
+    }
+?>
+
+</style>
+
+<div class="container">
+
 	<table class="cart-table">
         <thead>
             <tr>
@@ -12,7 +38,7 @@
 ?>
                 <tr>
                     <td><?= $cart_item['filename'] ?></td>
-                    <td class="button_td download" ><a href="?page=download_file&file_id=<?= $cart_item['id_media_file'] ?>" class="button_a download"><?= DOWNLOAD ?></a></td>
+                    <td class="button_td download" ><a href="?page=download_file&file_id=<?= $cart_item['id_media_file'] ?>" class="button_a download" target="_blank"><?= DOWNLOAD ?></a></td>
                 </tr>
 <?php
             }
@@ -20,3 +46,10 @@
         </tbody>
     </table>
 </div>
+
+
+<?php
+
+require_once('ClientBundle/views/layout/footer.php');
+
+?>
