4d6c509150f1b406a5916cd3b42f4618707be0c6
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-09-30 08:31:30 +0200

    temp commit

--- ToolBundle/models/WorkFlow.php
+++ ToolBundle/models/WorkFlow.php
@@ -120,7 +120,7 @@ class WorkFlowModel extends Model {
 		$row = $data->fetch_assoc();
 		if (isset($row['id_media'])) {
 			$mime = mime_content_type($filepath . $filename);
-			$data $this->_mysqli->query('INSERT INTO media_file (id_media, filename, filepath, right_download, right_preview, metadata, mime_type) VALUES ('. $row['id_media'] .', "'. $filename .'", "'. $filepath .'", '.$right_download.', '.$right_preview.', "'.$metadata.'", "'. $mime_type .'")');
+			$data = $this->_mysqli->query('INSERT INTO media_file (id_media, filename, filepath, right_download, right_preview, metadata, mime_type) VALUES ('. $row['id_media'] .', "'. $filename .'", "'. $filepath .'", '.$right_download.', '.$right_preview.', "'.$metadata.'", "'. $mime_type .'")');
 		}
 
 		return array(
