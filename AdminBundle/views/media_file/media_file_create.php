5eb6ef326462a3ceb301d4185d168aff4eac4f50
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-10-19 17:59:01 +0200

    fix

--- AdminBundle/views/media_file/media_file_create.php
+++ AdminBundle/views/media_file/media_file_create.php
@@ -7,6 +7,22 @@ require_once('ClientBundle/views/layout/header.php');
 	<link rel="stylesheet" href="AdminBundle/ressources/fine-uploader/fine-uploader-new.css">
 	<link rel="stylesheet" href="CoreBundle/ressources/layout/css/form.css">
 
+	<style>
+
+<?php
+	    if (isset($designs)) {
+
+	        foreach ($designs as $design) {
+?>
+	            <?= $design['selector'] ?> {
+	                <?= $design['property'] ?> : <?= $design['value'] ?>;
+	            }
+<?php
+	        }
+	    }
+?>
+
+	</style>
 
 	<script src="AdminBundle/ressources/fine-uploader/fine-uploader.js"></script>
 
