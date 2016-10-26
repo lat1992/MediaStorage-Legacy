1b1755cf179f95dcefbc2594978019741d7630cf
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-10-26 18:08:41 +0200

    graphical correction

--- ClientBundle/views/cart/cart_download_list.php
+++ ClientBundle/views/cart/cart_download_list.php
@@ -37,7 +37,7 @@ require_once('ClientBundle/views/layout/header.php');
             while ($cart_item = $cart_data->fetch_assoc()) {
 ?>
                 <tr>
-                    <td><?= $cart_item['filename'] ?></td>
+                    <td style="word-wrap: break-word;white-space: normal;" ><?= $cart_item['filename'] ?></td>
                     <td class="button_td download" ><a href="?page=download_file&file_id=<?= $cart_item['id_media_file'] ?>" class="button_a download" target="_blank"><?= DOWNLOAD ?></a></td>
                 </tr>
 <?php
