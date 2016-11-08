c88892ec7970feaabf7b65f364f5e0455b964233
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-11-09 00:59:17 +0100

    hotfix

--- AdminBundle/views/user/user_create.php
+++ AdminBundle/views/user/user_create.php
@@ -7,6 +7,23 @@ require_once('ClientBundle/views/layout/header.php');
 <link rel="stylesheet" href="CoreBundle/ressources/layout/css/form.css">
 <link rel="stylesheet" href="ClientBundle/ressources/content/css/button.css">
 
+<style>
+
+<?php
+    if (isset($designs)) {
+
+        foreach ($designs as $design) {
+?>
+            <?= $design['selector'] ?> {
+                <?= $design['property'] ?> : <?= $design['value'] ?>;
+            }
+<?php
+        }
+    }
+?>
+
+</style>
+
 <?php
 
 require_once('AdminBundle/views/user/user_create_form.php');
--- AdminBundle/views/user/user_list.php
+++ AdminBundle/views/user/user_list.php
@@ -3,6 +3,23 @@
 <link rel="stylesheet" href="CoreBundle/ressources/layout/css/form.css">
 <link rel="stylesheet" href="ClientBundle/ressources/content/css/button.css">
 
+<style>
+
+<?php
+    if (isset($designs)) {
+
+        foreach ($designs as $design) {
+?>
+            <?= $design['selector'] ?> {
+                <?= $design['property'] ?> : <?= $design['value'] ?>;
+            }
+<?php
+        }
+    }
+?>
+
+</style>
+
 <div id="container">
 
 	<a class="button button-add" href="?page=create_user_admin"><?= USER_CREATION_TITLE ?></a>
