2c56dee49123dc0eedd8f73242d16fc0029e59a1
Jean-Stephane VICTOR <jvict1@epitech.eu>
2016-11-09 11:10:52 +0100

    changing timecode format

--- ClientBundle/views/content/content.php
+++ ClientBundle/views/content/content.php
@@ -158,13 +158,13 @@ if (isset($current_media_file) && strpos($current_media_file['mime_type'], "vide
 				// timeArray = (time % 60).toFixed(2).toString().split(".");
 			 //    video_timer.innerHTML = min.toString() + ":" + timeArray[0] + ":" + timeArray[1];
 
-			 totalSec = video_player.currentTime.round();
+			 totalSec = Math.round(video_player.currentTime);
 
 				var hours = parseInt( totalSec / 3600 ) % 24;
 				var minutes = parseInt( totalSec / 60 ) % 60;
 				var seconds = totalSec % 60;
 
-				var result = (hours < 10 ? "0" + hours : hours) + "-" + (minutes < 10 ? "0" + minutes : minutes) + "-" + (seconds  < 10 ? "0" + seconds : seconds);
+				var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
 
 				video_timer.innerHTML = result
 			    video_timer_second.innerHTML = totalSec;
