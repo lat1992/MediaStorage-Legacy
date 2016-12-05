33812fdeb30c1a7896f541f3cb3516d60bf40158
Mau Hoi WU <mauhoi.wu@gmail.com>
2016-12-05 18:23:15 +0100

    fix dashboard

--- RootBundle/views/dashboard/dashboard.php
+++ RootBundle/views/dashboard/dashboard.php
@@ -7,7 +7,7 @@ require_once('RootBundle/views/layout/header.php');
 <?php
 		while ($organization = $organizations['data']->fetch_assoc()) {
 ?>
-			<a style="display: inline-block;text-decoration: none;line-height: 30px;background-color: #e5e5e5;padding: 5px;margin: 10px;color: black" href="http://<?= $organization['reference'] ?>.mediastoragekvi.fr?page=folder"><?= $organization['reference'] ?> / <?= $organization['organization_name'] ?> / <?= $organization['group_name'] ?></a><br/>
+			<a style="display: inline-block;text-decoration: none;line-height: 30px;background-color: #e5e5e5;padding: 5px;margin: 10px;color: black" href="http://<?= $organization['reference'] ?>.mediastorage.fr?page=folder"><?= $organization['reference'] ?> / <?= $organization['organization_name'] ?> / <?= $organization['group_name'] ?></a><br/>
 <?php
 		}
 ?>
